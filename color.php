8aba66c811ababb8868d7eb103e39a744597e447
Shane A. Workman <workma18@gmail.com>
2017-06-30 10:16:27 -0400

    Update color.php

--- color.php
+++ color.php
@@ -13,41 +13,32 @@
   $summer = array("red", "white", "blue");
   $date = date('m');
 
-    function myRandom($colors, $str) {
+    function myRandom($colors) {
         for ($i = 0; $i < 100; $i++) {
           $max = count($colors) - 1;
           $min = 0;
           $rand = rand($min, $max);
           print('
-          <div style="background-color : '.$colors[$rand].'; width : 10%; height:10%; float: left;">
-      <!--
-          <p style="position: absolute;  top: auto;">'.$str.'</p>
-            <br>Rand: '.$rand.'
-            <br>Color: '.$colors[$rand].'
-      -->
-          </div>');
+          <div style="background-color : '.$colors[$rand].'; width : 10%; height:10%; float: left;"></div>');
         }
     }
 
 
     if ($date == 01){
-      myRandom($newYears, 'Happy New Year');
+      myRandom($newYears);
       //print($date.' 1');
     } elseif ($date >= 5 && $date <= 9 ) {
-      myRandom($summer, 'It\'s Summer!');
+      myRandom($summer);
       //print($date.' 2');
     } elseif ($date == 10) {
-      myRandom($holloween, 'Holloween');
+      myRandom($holloween);
       //print($date.' 3');
     } elseif ($date == 12) {
-      myRandom($christmas, 'Merry Christmas');
+      myRandom($christmas);
       //print($date.' 4');
     } else {
       myRandom($colors);
       //print($date.' 5');
     }
-
-
-    // refreshes the page every 300ms.
-    print('<script> setTimeout(function() {        location.reload();        }, 300); </script>');
-  ?>
+?>
+<script> setTimeout(function() {        location.reload();        }, 300); </script>
